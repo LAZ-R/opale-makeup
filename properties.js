bcea2272f55352fcd21c76d8f67447c9c5c35e3d
LAZ-R <crazydrawer62@gmail.com>
2024-03-31 11:02:37 +0200

    v 0.1.12 - fix path 3

--- properties.js
+++ properties.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Opale Makeup';
-export const APP_VERSION = '0.1.11'; // v 0.1.11 - fix path2
+export const APP_VERSION = '0.1.12'; // v 0.1.12 - fix path 3
 export const APP_SHORT_NAME = `omuapp001`;
