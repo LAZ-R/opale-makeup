67ae3d38fc30774bcd748c04ec8d886da5814476
LAZ-R <crazydrawer62@gmail.com>
2024-03-31 11:42:26 +0200

    v 0.1.15 - fonts -> no rsms.me preconnect

--- properties.js
+++ properties.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Opale Makeup';
-export const APP_VERSION = '0.1.14'; // v 0.1.14 - optimisation photos accueil
+export const APP_VERSION = '0.1.15'; // v 0.1.15 - fonts -> no rsms.me preconnect
 export const APP_SHORT_NAME = `omuapp001`;
--- sw.js
+++ sw.js
@@ -25,4 +25,4 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
-const version = '0.1.14';
+const version = '0.1.15';
