e4dcd3c2bb819807027e861d55f686efee5e8f1c
LAZ-R <crazydrawer62@gmail.com>
2025-09-02 14:13:55 +0200

    v 0.2.0 - supression mains + fix taille icones footer en responsive

--- properties.js
+++ properties.js
@@ -1,3 +1,3 @@
 export const APP_NAME = 'Opale Makeup';
-export const APP_VERSION = '0.1.19'; // v 0.1.19 - photos semi-permanent uni
+export const APP_VERSION = '0.2.0'; // v 0.2.0 - supression mains + fix taille icones footer en responsive
 export const APP_SHORT_NAME = `omuapp001`;
